ebf6c105ed39046e6abecff015b2810699cbdad9
Tania Griffith <tania_lynne@yahoo.com>
2025-09-09 12:35:50 -0400

    remove skip button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -193,7 +193,7 @@ export default function RootLayout({
                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID || 'n9m6d1w8qz'}");
+              })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID || "n9m6d1w8qz"}");
             }
           `}
         </Script>
@@ -201,9 +201,6 @@ export default function RootLayout({
       <body
         className={`${poppins.className} ${TrickOrDead.variable} antialiased relative bg-black`}
       >
-        <a href="#main-content" className="skip-to-content">
-          Skip to main content
-        </a>
         <ImagePreloader images={criticalImages} priority />
         <WebVitalsTracker />
         <StarryBackground starCount={300} />

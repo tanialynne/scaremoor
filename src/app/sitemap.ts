24cf3022d89c18bba4a2512608b0f3afa65d5343
Tania Griffith <tania_lynne@yahoo.com>
2025-09-09 11:19:49 -0400

    update dynamic sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,6 @@
 import { MetadataRoute } from 'next'
 import Books from './constants/Books'
+import BlogPosts from './constants/BlogPosts'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://www.scaremoor.com'
@@ -43,6 +44,24 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/podcasts`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly' as const,
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly' as const,
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/quiz`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly' as const,
+      priority: 0.6,
+    },
+    {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),
       changeFrequency: 'yearly' as const,
@@ -50,13 +69,21 @@ export default function sitemap(): MetadataRoute.Sitemap {
     },
   ]
 
+  // Dynamic blog posts
+  const blogPages = BlogPosts.map((post) => ({
+    url: `${baseUrl}/blog/${post.slug}`,
+    lastModified: new Date(post.publishedDate),
+    changeFrequency: 'monthly' as const,
+    priority: 0.7,
+  }))
+
   // Dynamic book pages
   const bookPages = Books.map((book) => ({
     url: `${baseUrl}/book/${book.bookSlug}`,
     lastModified: new Date(),
     changeFrequency: 'monthly' as const,
-    priority: 0.9,
+    priority: 0.8,
   }))
 
-  return [...staticPages, ...bookPages]
+  return [...staticPages, ...blogPages, ...bookPages]
 }
